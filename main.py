86a7fe1a5302eabec86f50b04ddc2adab21f4769
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-10-11 21:09:00 -0600

    Documentación automática con Swagger

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/', tags=['home'])
+def message():
+    return "Hello world!"
