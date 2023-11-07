9e276849606c6c67f0a7893b55b43dd4b2c21a1e
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-11-06 22:22:51 -0600

    Servicios para consultar datos

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-
-from starlette.responses import JSONResponse
-
-from jwt_manager import create_token
 from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
+from routers.user import user_router
+
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 app.add_middleware(ErrorHandler)
+
 app.include_router(movie_router)
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
 
@@ -26,10 +26,3 @@ class User(BaseModel):
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
-
-
-@app.post('/login', tags=['auth'])
-def login(user: User):
-    if user.email == "admin@gmail.com" and user.password == "admin":
-        token: str = create_token(user.model_dump())
-        return JSONResponse(status_code=200, content=token)
--- routers/movie.py
+++ routers/movie.py
@@ -7,6 +7,7 @@ from config.database import Session
 from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
 from middlewares.jwt_bearer import JWTBearer
+from services.movie import MovieService
 
 movie_router = APIRouter()
 
@@ -36,14 +37,14 @@ class Movie(BaseModel):
                   dependencies=[Depends(JWTBearer())])
 def get_movies() -> JSONResponse:
     db = Session()
-    result = db.query(MovieModel).all()
+    result = MovieService(db).get_movies()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
 @movie_router.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> JSONResponse:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if not result:
         return JSONResponse(status_code=404, content={'message': "No encontrado"})
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
